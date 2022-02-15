5c7a4e6e1029654ef79482b8d6444cafacee66e2
ITigor1985 <xromer85@gmail.com>
2022-02-15 15:47:23 +0200

    123

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ import 'material-icons/iconfont/filled.css';
 import './sass/main.scss';
 
 // import './js/script';
-import './js/reviews';
-import './js/backdrop';
-import './js/video';
-import './js/slick';
-import './js/gallery'
-import './js/faq';
